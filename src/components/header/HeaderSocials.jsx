23c305a4f5e1008b73753884ee0874991c4967a8
Kchen1515 <99617496+Kchen1515@users.noreply.github.com>
2022-06-11 16:24:32 -0400

    link issue

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -6,9 +6,9 @@ import {BsYoutube} from 'react-icons/bs'
 const HeaderSocials = () => {
   return (
     <div className='header__socials'>
-        <a href='https://www.linkedin.com/in/kongyuchen/' target="_blank"><BsLinkedin/></a>
-        <a href='https://github.com/Kchen1515' target="_blank"><BsGithub/></a>
-        <a href='https://www.youtube.com/channel/UC2ENhwzCwWMpJhbuAIfXzfg' target="_blank"><BsYoutube/></a>
+        <a href='https://www.linkedin.com/in/kongyuchen/' target="_blank" rel="noreferrer"><BsLinkedin/></a>
+        <a href='https://github.com/Kchen1515' target="_blank" rel="noreferrer"><BsGithub/></a>
+        <a href='https://www.youtube.com/channel/UC2ENhwzCwWMpJhbuAIfXzfg' target="_blank" rel="noreferrer"><BsYoutube/></a>
     </div>
   )
 }
